d3fff2b01798910f6454debc8906face6e6f00ed
suyogbhat <55197604+suyogbhat8@users.noreply.github.com>
2020-02-14 10:47:24 +0530

    Update dij.py

--- dij.py
+++ dij.py
@@ -50,7 +50,6 @@ def Creatematrix(fname):   ## function to take text file as input and print it i
 
     return Graph
 
-
 def dijkstra(Graph, Start):    ##function to perform dijkstra algorithm
     global distances
     distances = {}
@@ -99,18 +98,43 @@ def printpath(Start,previous,destination):   ##function to print previous path
         x = previous[x]
     path = path + str(Start)
     print path[::-1]
- 
- 
- 
- 
- 
- 
- 
-fname=raw_input("Input Original Network Topology Matrix File :") ####main funtion for option 1 ,matrix printed
 
-G=Creatematrix(fname)
-print("enter 1 to see route")
-if input()=='1':
+
+###.................Actual Program............Starts.................................
+print "........................................................................"
+#print "Welcome to Link State Routing Simulator"
+print "1->Input the text file and print it in matrix format"
+#print "2->To print the connection table of your source router"
+print "3->To print the total cost,shortest path between your source and destination router "
+#print "4->To delete a router"
+#print "5->To print the best router"
+#print "6->Program Exit"
+print "Press Y to return to Main Menu"
+print "............................................................................"
+c=1
+while True:   ##infinity loop
+  try:
+   n = (int(raw_input("Master Command:")))
+  except :
+      print "Enter Number Only...Exiting......try again..."
+      quit()
+
+  if n!=1 and c==1:                   ##to validate that no other options can be executed before option 1
+    print "Please insert the matrix first"
+    continue
+  else:
+    c=0
+
+  if n==1:
+    fname=raw_input("Input Original Network Topology Matrix File :") ####main funtion for option 1 ,matrix printed
+
+    G=Creatematrix(fname)
+  ## connectiontable called to print the coonection table
+
+  elif n==3:
+                             ## to show cost and the best path
+
+
     try:
 
      Start1 = int(raw_input("Enter Start Router:"))
@@ -126,7 +150,13 @@ if input()=='1':
         if v == destination:
             print Start1, '->', destination
             print 'Cost:',dist_A[v]
-    printpath(Start1, previous, destination)
-    
-else:
-    print("invalid input")
+    printpath(Start1, previous, destination) ##printpath
+ 
+  elif n>6:                     ##to check options dont exceed 6
+      print "Options Exceeded....."
+      break
+  return_to_menu = raw_input("Return to Main Menu,y/n:")
+  if return_to_menu != 'y':
+
+      print "Sorry ,Wrong Choice.....The program terminates......"
+      break
